33ecff30c768c963014266dce7d922e957bcb198
mathias_gd <154527148+matheusgamerXdc@users.noreply.github.com>
2023-12-21 10:22:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function bodyLoad()
+{
+  document.getElementById("bt").style.fontSize = "15px";
+  document.getElementById("bt").style.backgroundColor="darkred";
+  document.getElementById("bt").style.borderRadius="20px";
+  document.getElementById("bt").style.color="white";
+  document.getElementById("bt").style.padding="10px";
+  document.getElementById("bt").style.boderStyle="none";
+  
+}
+function ChangeTheme()
+{
+  document.getElementById("body").style.backgroundColor="white";
+  
+  document.getElementById("nomeTitulo").style.backgroundColor="darkorange";
+  
+  document.getElementById("anchorTag").style.color="darkblue";
+  
+  document.getElementById("anchorTag").style.boderWidth="0px"
+  
+}
